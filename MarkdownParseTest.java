44d88e5d6c0025bc55d9a30e0f077e8c47fb8b34
michaeLeung <mtleung@ucsd.edu>
2022-02-02 19:44:26 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,10 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.ArrayList;
+
 
 public class MarkdownParseTest {
     @Test
@@ -13,10 +13,29 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void parse() throws IOException {
+        ArrayList<String>[] list = new ArrayList[4];
+        list[0] = new ArrayList();
+        list[1] = new ArrayList();
+        list[2] = new ArrayList();
+        list[3] = new ArrayList();
+        list[0].add("http://something.com");
+        list[0].add("http://somemorething.com");
+        list[1].add("https://something.com");
+        list[1].add("some-page.html");
+        list[2].add("www.something.com");
+        list[2].add("www.something.com");
+        
+
+        for (int i = 0; i < 4; i++) {
+            String file = load("markdown" + (i+1) + ".md");
+            assertEquals(list[i], MarkdownParse.getLinks(file));
+        }
     }
-    
+
+    private String load(String words) throws IOException {
+		Path fileName = Path.of(words);
+	    String contents = Files.readString(fileName);
+        return contents;
+    }
 }
