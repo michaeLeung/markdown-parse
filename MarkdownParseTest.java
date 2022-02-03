6ad61e6ab4eb7f5b66654d7e957ac4f0e40cc708
michaeLeung <mtleung@ucsd.edu>
2022-02-02 19:11:56 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -25,7 +25,6 @@ public class MarkdownParseTest {
         list[1].add("some-page.html");
         list[2].add("www.something.com");
         list[2].add("www.something.com");
-        list[3].add("");
         
 
         for (int i = 0; i < 4; i++) {
